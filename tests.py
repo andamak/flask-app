9962f7be6e04b3df12284200f977c5763a3b9225
Алексей К <andamak@yandex.ru>
2021-01-28 11:14:58 +0300

    anothe answer of server 3

--- tests.py
+++ tests.py
@@ -9,7 +9,8 @@ class FlaskAppTests(unittest.TestCase):
 
     def test_get(self):
         r = self.app.get('/')
-        self.assertEqual(r.data.decode('utf-8'), 'У меня получилось!? Работает 2021 01 28 10:58')
+        self.assertEqual(r.data.decode('utf-8'), 'У меня получилось!? \
+            Работает 2021 01 28 10:58')
 
     def test_post(self):
         r = self.app.post('/')
