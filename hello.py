9eb662d96ac15f8b272f4087400061741689ddb8
Алексей К <andamak@yandex.ru>
2021-01-28 10:57:57 +0300

    anothe answer of server

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'У меня получилось!? акуеть'
+    return 'У меня получилось!? Работает 2021 01 28 10:58'
 
 
 if __name__ == '__main__':
