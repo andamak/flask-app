a97e740637228635a5d33c8fc7a383f0b8c6abc2
Алексей К <andamak@yandex.ru>
2021-01-28 11:36:13 +0300

    change message

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'У меня получилось!? Работает 2021 01 28 10:58'
+    return 'У меня получилось!? Работает 2021 01 28 11:35'
 
 
 if __name__ == '__main__':
